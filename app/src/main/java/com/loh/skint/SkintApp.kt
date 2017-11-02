ea4c1a3867715359a73e61622ab101befda0a3ec
liam <liamoh@outlook.com>
2017-11-02 23:24:00 +0000

    Adds floating action button for account creation.

--- app/src/main/java/com/loh/skint/SkintApp.kt
+++ app/src/main/java/com/loh/skint/SkintApp.kt
@@ -1,5 +1,20 @@
 package com.loh.skint
 
 import android.app.Application
+import com.facebook.stetho.Stetho
+import com.squareup.leakcanary.LeakCanary
+import timber.log.Timber
 
-class SkintApp : Application()
+class SkintApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) return
+        LeakCanary.install(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+            Stetho.initializeWithDefaults(this)
+        }
+    }
+}
