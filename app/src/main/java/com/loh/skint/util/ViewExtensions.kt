69975b0e6b789a3aa8b821a451d2a0e68a540e59
liam <liamoh@outlook.com>
2017-11-10 22:37:11 +0000

    Fixes view.hide extension not actually hiding views.

--- app/src/main/java/com/loh/skint/util/ViewExtensions.kt
+++ app/src/main/java/com/loh/skint/util/ViewExtensions.kt
@@ -4,29 +4,25 @@ import android.support.annotation.LayoutRes
 import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
-import timber.log.Timber
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int): View =
         LayoutInflater.from(context).inflate(layoutRes, this, false)
 
 fun ViewPager.scrollLeft() {
-    val index = if (currentItem == 0) adapter.count else currentItem--
-    Timber.d("Scroll Left: $index")
-    setCurrentItem(index, true)
+    if (currentItem > 0) setCurrentItem(currentItem - 1, true)
 }
 
 fun ViewPager.scrollRight() {
-    val index = if (currentItem == adapter.count) currentItem-- else 0
-    Timber.d("Scroll Right: $index")
-    setCurrentItem(index, true)
+    if (currentItem < adapter.count) setCurrentItem(currentItem + 1, true)
 }
 
-inline fun View.show() {
+fun View.show() {
     visibility = VISIBLE
 }
 
-inline fun View.hide() {
-    visibility = VISIBLE
+fun View.hide() {
+    visibility = GONE
 }
