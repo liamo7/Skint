af8c02dbf073a5b81d44c99e110ab3be2b42c1f3
liam <liamoh@outlook.com>
2017-11-28 16:37:40 +0000

    Fixes date range preserving issue.

--- app/src/main/java/com/loh/skint/ui/record/list/RecordListActivity.kt
+++ app/src/main/java/com/loh/skint/ui/record/list/RecordListActivity.kt
@@ -18,16 +18,38 @@ class RecordListActivity : BaseAccountDrawerActivity() {
 
     @Inject lateinit var pagerAdapter: RecordListPagerAdapter
 
-    private lateinit var dateRangeDialog: MaterialDialog
     private lateinit var date: LocalDate
+    private val dateRangeDialog: MaterialDialog by lazy {
+        MaterialDialog.Builder(this)
+                .title(R.string.title_date_range_select)
+                .items(R.array.date_ranges)
+                .itemsCallbackSingleChoice(pagerAdapter.getDateRange().id, { _, _, which, _ ->
+                    // update our newly selected date range
+                    pagerAdapter.setDateRange(DateRange.values()[which])
+                    // update the pager position to new position of date based on new date range
+                    viewpager.currentItem = date.calculateViewpagerPositionFromDateRange(pagerAdapter.getDateRange())
+                    true
+                })
+                .positiveText(R.string.choose)
+                .build()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (savedInstanceState != null && savedInstanceState[ARG_STATE] != null) {
+            pagerAdapter.setDateRange(savedInstanceState.getSerializable(ARG_STATE) as DateRange)
+        }
 
         // determines whether we should navigate to a specific date in time for list
         date = intent.getSerializableExtra(INTENT_DATE) as LocalDate? ?: LocalDate.now()
         viewpager.adapter = pagerAdapter
         viewpager.currentItem = date.calculateViewpagerPositionFromDateRange(pagerAdapter.getDateRange())
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(ARG_STATE, pagerAdapter.getDateRange())
     }
 
     override fun getLayoutRes(): Int = R.layout.activity_record_list
@@ -47,19 +69,6 @@ class RecordListActivity : BaseAccountDrawerActivity() {
     }
 
     private fun openDateRangeDialog() {
-        dateRangeDialog = MaterialDialog.Builder(this)
-                .title(R.string.title_date_range_select)
-                .items(R.array.date_ranges)
-                .itemsCallbackSingleChoice(pagerAdapter.getDateRange().id, { _, _, which, _ ->
-                    // update our newly selected date range
-                    pagerAdapter.setDateRange(DateRange.values()[which])
-                    // update the pager position to new position of date based on new date range
-                    viewpager.currentItem = date.calculateViewpagerPositionFromDateRange(pagerAdapter.getDateRange())
-                    true
-                })
-                .positiveText(R.string.choose)
-                .build()
-
-        if (!dateRangeDialog.isShowing) dateRangeDialog.show()
+        dateRangeDialog.show()
     }
 }
